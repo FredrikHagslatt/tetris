5175ae215ba912363a1dd0efe9522dac66580f43
Fredrik Hagslätt <fredrik.hagslatt@gmail.com>
2021-06-16 22:25:48 +0200

    SideHeldThreshold to 0.15 seconds.

--- MultiplayerTetris/TetrisPlayer.h
+++ MultiplayerTetris/TetrisPlayer.h
@@ -25,7 +25,7 @@ private:
 
 	bool sideHeldStarted = 0.0f;
 	float sideHeldTimer = 0.0f;
-	const float sideHeldThreshold = 0.6f;
+	const float sideHeldThreshold = 0.15f;
 	const float sideHeldSpeed = 0.05f;
 
 	int xPos = 5;
